9fda5bfc930b135e775b645614694664783c10e3
Code Terra <76623307+codeterrayt@users.noreply.github.com>
2024-07-19 16:22:35 +0530

    add: auth, livewire, light-dark mode support

--- Laravel/resources/js/app.js
+++ Laravel/resources/js/app.js
@@ -1,1 +1,69 @@
-import './bootstrap';
+import "./bootstrap";
+
+// import Alpine from "alpinejs";
+
+
+
+
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+// DARK MODE TOGGLE BUTTON
+var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+    themeToggleLightIcon.classList.remove("hidden");
+} else {
+    themeToggleDarkIcon.classList.remove("hidden");
+}
+
+
+var themeToggleBtn = document.getElementById("theme-toggle");
+
+
+function update_theme(){
+    if (localStorage.getItem("color-theme")) {
+        if (localStorage.getItem("color-theme") === "light") {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+        }
+    } else {
+        if (document.documentElement.classList.contains("dark")) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+        }
+    }
+}
+
+themeToggleBtn.addEventListener("click", function () {
+    themeToggleDarkIcon.classList.toggle("hidden");
+    themeToggleLightIcon.classList.toggle("hidden");
+
+    update_theme();
+});
+
+
+if (localStorage.getItem("color-theme")) {
+        document.documentElement.classList.add(localStorage.getItem("color-theme"));
+} else {
+    if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+    } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+    }
+}
