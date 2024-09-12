12d55851dbb47df729b32e9f99d63e589e792c25
Code Terra <76623307+codeterrayt@users.noreply.github.com>
2024-09-12 18:31:03 +0530

    download, pull, exists

--- NodeJs Server/docker.js
+++ NodeJs Server/docker.js
@@ -1,9 +1,11 @@
 const Docker = require("dockerode");
 const { json } = require("express");
-const path = require('path');
+const path = require("path");
+const axios = require("axios");
+const { env } = require("process");
 
 // Load constants synchronously
-const CONSTANTS = require(path.join(__dirname, 'constants', 'constants.json'));
+const CONSTANTS = require(path.join(__dirname, "constants", "constants.json"));
 
 // Create a Dockerode instance with default settings
 const docker = new Docker();
@@ -16,12 +18,16 @@ const inspectContainerWithRetry = async (container, ports) => {
   const containerInfo = await container.inspect();
   const portMappings = containerInfo.NetworkSettings.Ports;
 
-  let newMap = {"portMap":{},"container_id":containerInfo.Id, "container_state":containerInfo.State};
+  let newMap = {
+    portMap: {},
+    container_id: containerInfo.Id,
+    container_state: containerInfo.State,
+  };
 
   for (port of ports) {
     try {
       const mappedPort = portMappings[`${port}/tcp`][0].HostPort;
-      newMap['portMap'][String(port)] = mappedPort;
+      newMap["portMap"][String(port)] = mappedPort;
     } catch (error) {
       console.log(error);
     }
@@ -30,22 +36,27 @@ const inspectContainerWithRetry = async (container, ports) => {
   return newMap;
 };
 
-const startContainer = async (container_id,ports) => {
+const startContainer = async (container_id, ports) => {
   try {
     const container = docker.getContainer(container_id);
 
     if (!container) {
-      return json({ "success": false, "message": CONSTANTS.CONTAINER_NOT_FOUND });
+      return json({ success: false, message: CONSTANTS.CONTAINER_NOT_FOUND });
     }
 
     // Inspect the container to check if it exists
     const inspectData = await new Promise((resolve, reject) => {
       container.inspect((err, data) => {
         if (err || !data) {
-          return reject({ "success": false,"message": CONSTANTS.CONTAINER_NOT_FOUND });
-        }
-        else if(!data.State.Paused){
-          return reject({ "success": false, "message": CONSTANTS.CONTAINER_ALREADY_RUNNING_STATE });
+          return reject({
+            success: false,
+            message: CONSTANTS.CONTAINER_NOT_FOUND,
+          });
+        } else if (!data.State.Paused) {
+          return reject({
+            success: false,
+            message: CONSTANTS.CONTAINER_ALREADY_RUNNING_STATE,
+          });
         }
         resolve(data);
       });
@@ -55,36 +66,47 @@ const startContainer = async (container_id,ports) => {
     await new Promise((resolve, reject) => {
       container.restart((err, data) => {
         if (err) {
-          return reject({ "success": false,"message": CONSTANTS.CONTAINER_NOT_FOUND });
+          return reject({
+            success: false,
+            message: CONSTANTS.CONTAINER_NOT_FOUND,
+          });
         }
         resolve(data);
       });
     });
 
-    const icwr = await inspectContainerWithRetry(container,ports);
-    return { "success": true, "message": CONSTANTS.CONTAINER_RESTARTED_SUCCESSFULLY, "portMap": icwr.portMap};
-
+    const icwr = await inspectContainerWithRetry(container, ports);
+    return {
+      success: true,
+      message: CONSTANTS.CONTAINER_RESTARTED_SUCCESSFULLY,
+      portMap: icwr.portMap,
+    };
   } catch (error) {
-    return { "success": false, "message": error.message };
+    return { success: false, message: error.message };
   }
 };
 
-const pauseContainer = async (container_id,ports) => {
+const pauseContainer = async (container_id, ports) => {
   try {
     const container = docker.getContainer(container_id);
 
     if (!container) {
-      return json({ "success": false, "message": CONSTANTS.CONTAINER_NOT_FOUND });
+      return json({ success: false, message: CONSTANTS.CONTAINER_NOT_FOUND });
     }
 
     // Inspect the container to check if it exists
     const inspectData = await new Promise((resolve, reject) => {
       container.inspect((err, data) => {
         if (err || !data) {
-          return reject({ "success": false,"message": CONSTANTS.CONTAINER_NOT_FOUND });
-        }
-        else if(data.State.Paused){
-          return reject({ "success": false, "message": CONSTANTS.CONTAINER_ALREADY_PAUSE_STATE });
+          return reject({
+            success: false,
+            message: CONSTANTS.CONTAINER_NOT_FOUND,
+          });
+        } else if (data.State.Paused) {
+          return reject({
+            success: false,
+            message: CONSTANTS.CONTAINER_ALREADY_PAUSE_STATE,
+          });
         }
         resolve(data);
       });
@@ -94,34 +116,42 @@ const pauseContainer = async (container_id,ports) => {
     await new Promise((resolve, reject) => {
       container.pause((err, data) => {
         if (err) {
-          return reject({ "success": false,"message": CONSTANTS.CONTAINER_NOT_FOUND });
+          return reject({
+            success: false,
+            message: CONSTANTS.CONTAINER_NOT_FOUND,
+          });
         }
         resolve(data);
       });
     });
 
-    const icwr = await inspectContainerWithRetry(container,ports);
-    return { "success": true, "message": CONSTANTS.CONTAINER_PAUSED_SUCCESSFULLY , "portMap": icwr.portMap};
-
+    const icwr = await inspectContainerWithRetry(container, ports);
+    return {
+      success: true,
+      message: CONSTANTS.CONTAINER_PAUSED_SUCCESSFULLY,
+      portMap: icwr.portMap,
+    };
   } catch (error) {
-    return { "success": false, "message": error.message };
+    return { success: false, message: error.message };
   }
 };
-
 
 const stopContainer = async (container_id) => {
   try {
     const container = docker.getContainer(container_id);
 
     if (!container) {
-      return json({ "success": false, "message": CONSTANTS.CONTAINER_NOT_FOUND });
+      return json({ success: false, message: CONSTANTS.CONTAINER_NOT_FOUND });
     }
 
     // Inspect the container to check if it exists
     const inspectData = await new Promise((resolve, reject) => {
       container.inspect((err, data) => {
         if (err || !data) {
-          return reject({ "success": false,"message": CONSTANTS.CONTAINER_NOT_FOUND });
+          return reject({
+            success: false,
+            message: CONSTANTS.CONTAINER_NOT_FOUND,
+          });
         }
         resolve(data);
       });
@@ -131,41 +161,118 @@ const stopContainer = async (container_id) => {
     await new Promise((resolve, reject) => {
       container.stop((err, data) => {
         if (err) {
-          return reject({ "success": false,"message": CONSTANTS.CONTAINER_NOT_FOUND });
+          return reject({
+            success: false,
+            message: CONSTANTS.CONTAINER_NOT_FOUND,
+          });
         }
         resolve(data);
       });
     });
 
-    return { "success": true, "message": CONSTANTS.CONTAINER_STOPPED_SUCCESSFULLY };
-
+    return { success: true, message: CONSTANTS.CONTAINER_STOPPED_SUCCESSFULLY };
   } catch (error) {
-    return { "success": false, "message": error.message };
+    return { success: false, message: error.message };
   }
 };
 
+const is_image_exists = async (image_repo_name) => {
+  return new Promise((resolve) => {
+    docker.getImage(image_repo_name).inspect((err, data) => {
+      if (err && err.statusCode === 404) {
+        resolve({
+          exists: false,
+          success: true
+        });
+      } else if (err) {
+        resolve({
+          exists: false,
+          success: false
+        });
+      } else {
+        resolve({
+          exists: true,
+          success: true
+        });
+      }
+    });
+  });
+};
+
+
+const pull_image_in_background = async (image_repo_name) => {
+  // Start pulling the image in the background
+  // console.log(image_repo_name)
+  docker.pull(image_repo_name, (err, stream) => {
+    if (err) {
+      console.error(`Error pulling image: ${err.message}`);
+      return;
+    }
+
+    // Handle the stream in the background
+    docker.modem.followProgress(stream, async (err, output) => {
+      if (err) {
+        console.error(`Error during image pull process: ${err.message}`);
+        return;
+      }
+
+      // Once the image is fully pulled, make the API call
+      try {
+        let res = await axios.post(`${env.LARAVEL_URL}/api/image/pulled`, {
+          image_repo_name: btoa(image_repo_name)
+        });
+        console.log(res.data)
+        console.log(`Image ${image_repo_name} pulled and API notified.`);
+      } catch (apiError) {
+        console.error(`Error calling API after image pull: ${apiError}`);
+      }
+    });
+  });
+
+  // Immediately return a response indicating the image is being pulled
+  return {
+    pulling: true,
+    message: `Image ${image_repo_name} is being pulled in the background.`
+  };
+};
+
+// Example usage in an API route
+const pull_image = async (image_repo_name) => {
+  
+  try {
+    const result = await pull_image_in_background(image_repo_name);
+    return result;  // Send pulling response immediately
+  } catch (err) {
+    return {
+      success: false,
+      error: 'Error starting image pull process'
+    };
+  }
+};
 
 // Function to create and start the container
 const createAndStartContainer = async (image, ports, password) => {
-  //   const options = {
-  //     Image: "kasmweb/core-ubuntu-focal:1.14.0",
-  //     Tty: true,
-  //     Interactive: true,
-  //     Env: ["VNC_PW=password"],
-  //     HostConfig: {
-  //       AutoRemove: true,
-  //       ShmSize: 512 * 1024 * 1024, // 512 MB
-  //       PortBindings: {
-  //         "6901/tcp": [{ HostPort: "" }], // Docker will assign a random port
-  //       },
+  // const options = {
+  //   Image: "kasmweb/core-ubuntu-focal:1.14.0",
+  //   Tty: true,
+  //   Interactive: true,
+  //   Env: ["VNC_PW=password"],
+  //   HostConfig: {
+  //     AutoRemove: true,
+  //     ShmSize: 512 * 1024 * 1024, // 512 MB
+  //     PortBindings: {
+  //       "6901/tcp": [{ HostPort: "" }], // Docker will assign a random port
   //     },
-  //     User: "root", // Run as root
-  //   };
+  //   },
+  //   User: "root", // Run as root
+  // };
 
   let portBindings = {};
   ports.forEach((port) => {
     portBindings[`${port}/tcp`] = [{ HostPort: "" }];
   });
+
+  console.log(portBindings);
 
   const options = {
     Image: image,
@@ -188,10 +295,19 @@ const createAndStartContainer = async (image, ports, password) => {
   } catch (err) {
     // console.error("Error creating or starting container:", err);
     return json({
-      "success":false,
-      "message": `Ivalid Image ${image} ${ports} `,
+      success: false,
+      message: `Ivalid Image ${image} ${ports} `,
     });
   }
 };
 
-module.exports = {CONSTANTS, inspectContainerWithRetry, createAndStartContainer, startContainer,pauseContainer, stopContainer };
+module.exports = {
+  CONSTANTS,
+  inspectContainerWithRetry,
+  createAndStartContainer,
+  startContainer,
+  pauseContainer,
+  stopContainer,
+  is_image_exists,
+  pull_image
+};
--- NodeJs Server/index.js
+++ NodeJs Server/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
-const { createAndStartContainer, startContainer,pauseContainer, stopContainer, CONSTANTS } = require("./docker");
+const {
+  createAndStartContainer,
+  startContainer,
+  pauseContainer,
+  stopContainer,
+  is_image_exists,
+  pull_image,
+  CONSTANTS,
+} = require("./docker");
 
 //application level middleware
 app.use((req, res, next) => {
@@ -8,46 +16,64 @@ app.use((req, res, next) => {
   next();
 });
 
-
 app.use(express.json());
 
 app.all("/spawn/container", async (req, res) => {
-    const { ports, image, password } = req.body;
+  const { ports, image, password } = req.body;
 
-    if (!ports || !image || !password) {
-        return res.status(400).send({success:false,message:CONSTANTS.IMAGE_PORT_REQUIRED});
-    }
+  if (!ports || !image || !password) {
+    return res
+      .status(400)
+      .send({ success: false, message: CONSTANTS.IMAGE_PORT_REQUIRED });
+  }
 
-    try {
-        const container = await createAndStartContainer(image, ports , password);
-        if(container.message == undefined || container.message === null){
-          return res.status(500).send(container);
-        }
-        return res.json(container);
-    } catch (err) {
-        console.error("Failed to start container:", err.message);
-        return res.status(500).send({"message":"Failed to start container, Image Not Found"});
+  try {
+    console.log(image, ports, password);
+    const container = await createAndStartContainer(image, ports, password);
+    if (container.message == undefined || container.message === null) {
+      return res.status(500).send(container);
     }
+    console.log(message);
+    return res.json(container);
+  } catch (err) {
+    console.error("Failed to start container:", err.message);
+    return res
+      .status(500)
+      .send({ message: "Failed to start container, Image Not Found" });
+  }
 });
 
-app.all("/start/container",async (req,res)=>{
+app.all("/start/container", async (req, res) => {
   const { container_id, ports } = req.body;
-  const container = await startContainer(container_id,ports);
+
+  const container = await startContainer(container_id, ports);
   return res.send(container);
 });
 
-app.all("/pause/container",async (req,res)=>{
+app.all("/pause/container", async (req, res) => {
   const { container_id, ports } = req.body;
-  const container = await pauseContainer(container_id,ports);
+  const container = await pauseContainer(container_id, ports);
   return res.send(container);
 });
 
-app.all("/stop/container",async (req,res)=>{
+app.all("/stop/container", async (req, res) => {
   const { container_id } = req.body;
   const container = await stopContainer(container_id);
   return res.send(container);
 });
 
+app.all("/image/exists", async (req, res) => {
+  const { image_repo_name } = req.body;
+  // console.log(image_repo_name)
+  return res.send(await is_image_exists(image_repo_name));
+});
+
+app.all("/image/pull", async (req, res) => {
+  const { image_repo_name } = req.body;
+  let output = await pull_image(image_repo_name);
+  console.log(output);
+  return res.send(output);
+});
 
 app.use("/app", async (req, res, next) => {
   console.log("middlware");
