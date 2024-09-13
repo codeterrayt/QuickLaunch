343530c5d39fbc76ad4dae8c38d2c486aeb9c001
Code Terra <76623307+codeterrayt@users.noreply.github.com>
2024-09-13 15:10:19 +0530

    add: default username

--- Laravel/resources/views/livewire/home.blade.php
+++ Laravel/resources/views/livewire/home.blade.php
@@ -1,8 +1,10 @@
 <div>
 
-
-
-
+    <div class="container">
+        <h1 style="line-height: 40px;">username: kasm_user </h1>
+    </div>
+    <hr>
+    &nbsp;
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4" wire:poll.500ms>
         @foreach ($images as $image)
             <div
